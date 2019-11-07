c8cfa901e095082fa65abb1071cc32da5606e92e
Khang Ho <themarinect@gmail.com>
2019-11-07 02:12:42 -0500

    Create TradableItems.java

---
+++ Inferno/src/models/TradableItems.java
@@ -0,0 +1,27 @@
+package models;
+
+import java.util.ArrayList;
+
+public class TradableItems extends Item
+{
+	private String NPC;
+	
+	public TradableItems()
+	{
+		
+	}
+
+	public TradableItems(String _itemID, String _itemName, String _itemType, String[] _itemDesc,
+			ArrayList<String> _itemLocation, String _NPC)
+	{
+		super(_itemID, _itemName, _itemType, _itemDesc, _itemLocation);
+		this.NPC = _NPC;
+	}
+
+	//getter
+	public String getNPC()
+	{
+		return NPC;
+	}
+
+}
