cd20b67b2e4d4f4995bff84d2bf107d71a59b2e0
Khang Ho <themarinect@gmail.com>
2019-11-07 02:12:32 -0500

    Create HealingItems.java

---
+++ Inferno/src/models/HealingItems.java
@@ -0,0 +1,28 @@
+package models;
+
+import java.util.ArrayList;
+
+public class HealingItems extends Item
+{
+
+	private int strength;
+	
+	public HealingItems()
+	{
+		
+	}
+
+	public HealingItems(String _itemID, String _itemName, String _itemType, String[] _itemDesc,
+			ArrayList<String> _itemLocation, int _strength)
+	{
+		super(_itemID, _itemName, _itemType, _itemDesc, _itemLocation);
+		this.strength = _strength;
+	}
+
+	//getters
+	public int getStrength()
+	{
+		return strength;
+	}
+
+}
