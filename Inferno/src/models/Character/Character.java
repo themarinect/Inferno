60ea26017a98709b86d60d2eaaf0141e99f5a332
Khang Ho <themarinect@gmail.com>
2019-11-08 02:14:33 -0500

    Update Character class

--- Inferno/src/models/Character/Character.java
+++ Inferno/src/models/Character/Character.java
@@ -3,9 +3,38 @@ package models.Character;
 public class Character
 {
 
+	private String id;
+	private String name;
+	
 	public Character()
 	{
-
+		
+	}
+	
+	public Character(String _id, String _name)
+	{
+		this.id = _id;
+		this.name = _name;
 	}
 
+	//getters and setters
+	public String getId()
+	{
+		return id;
+	}
+
+	public void setId(String id)
+	{
+		this.id = id;
+	}
+
+	public String getName()
+	{
+		return name;
+	}
+
+	public void setName(String name)
+	{
+		this.name = name;
+	}
 }
