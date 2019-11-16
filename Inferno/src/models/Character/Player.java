5fe395ea2e1b3ad5c833b79bbc954edbac1c1b9f
Khang Ho <themarinect@gmail.com>
2019-11-16 18:08:12 -0500

    Change data type of HP and weapon for data binding

--- Inferno/src/models/Character/Player.java
+++ Inferno/src/models/Character/Player.java
@@ -2,6 +2,10 @@ package models.Character;
 
 import java.util.ArrayList;
 
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
 import models.Item.Item;
 
 
@@ -10,41 +14,69 @@ public class Player extends Character
 {
 	
 	private String currentRoom;
-	private int healthPoints;
-	//private String weapon = "";
+	private SimpleIntegerProperty HP;
+	private SimpleStringProperty weapon;
 	private ArrayList<Item> inventory = new ArrayList<Item>();
 	
 	public Player()
 	{
-		currentRoom = "R14";
-		healthPoints = 100;
+		HP = new SimpleIntegerProperty(100);
+		setWeapon(new SimpleStringProperty("Sword"));
 	}
 	
-	public Player(String _id, String _name, String _currentRoom, int _healthPoints)
+	public Player(String _id, String _name, String _currentRoom, SimpleIntegerProperty _HP)
 	{
 		super(_id, _name);
 		this.currentRoom = _currentRoom;
-		this.healthPoints = _healthPoints;
+		this.HP = _HP;
 	}
 
+	public IntegerProperty HPProperty()
+	{
+		return HP;
+	}
+	
+	public void displayInventory()
+	{
+		System.out.println("Inventory: ");
+		for(int i = 0; i < inventory.size(); i++)
+		{
+			if(inventory.size() == 0)
+				System.out.println("You have nothing in the inventory.\n");
+			else
+				System.out.println(inventory.get(i).getItemName() + ": " + inventory.get(i).getItemDesc() + "\n");
+		}
+	}
+	
+	//getters & setters
 	public String getCurrentRoom()
 	{
 		return currentRoom;
 	}
 
-	public int getHealthPoints()
-	{
-		return healthPoints;
-	}
-
-	public void setHealthPoints(int healthPoints)
-	{
-		this.healthPoints = healthPoints;
-	}
-
 	public ArrayList<Item> getInventory()
 	{
 		return inventory;
+	}
+	
+	public SimpleIntegerProperty getHP()
+	{
+		return HP;
+	}
+
+	public void setHP(SimpleIntegerProperty hP)
+	{
+		HP = hP;
+	}
+
+	public SimpleStringProperty getWeapon()
+	{
+		return weapon;
+	}
+
+	public void setWeapon(SimpleStringProperty weapon)
+	{
+		this.weapon = weapon;
 	}
 
 }
