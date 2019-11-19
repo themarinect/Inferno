d870e28b07ecbd52d9ae7574f34fd3d6319be769
Khang Ho <themarinect@gmail.com>
2019-11-19 02:56:57 -0500

    Update Item class

--- Inferno/src/models/Item/Item.java
+++ Inferno/src/models/Item/Item.java
@@ -47,6 +47,148 @@ public class Item
 		}
 	}
 	
+//	public static <T extends Item> T readItem(BufferedReader reader)
+//	{
+//		Item item = new Item();
+//		try
+//		{
+//			String line = reader.readLine();
+//			if(line == null)
+//				return null;
+//			item.setItemID(line);
+//			item.setItemName(reader.readLine());
+//			item.setItemType(reader.readLine());
+//			
+//			//reads description
+//			line = reader.readLine();
+//			ArrayList<String> itemDescList = new ArrayList<String>(); 
+//			String[] temp = null;
+//			itemDescList.add(line);
+//			temp = new String[itemDescList.size()];
+//			int i = 0;
+//			for(String descLine : itemDescList)
+//				temp[i++] = descLine;
+//			item.setItemDesc(temp);
+//			
+//			//reads location(s)
+//			String data = reader.readLine();
+//			String[] location = data.split("/",-2);
+//			ArrayList<String> locationList = new ArrayList<>();
+//			for(String a : location)
+//				locationList.add(a);
+//			item.setItemLocation(locationList);
+//			
+//			/*-------------------------------------------------------*/
+//			
+//			if(item instanceof CombatItem)
+//			{
+//				//reads usage
+//				while(true)
+//				{
+//					line = reader.readLine();
+//					if(line == null || line.equals(""))
+//						break;
+//					line = line.trim();
+//					int colon = line.indexOf(":");
+//					((CombatItem) item).setUsage(line.substring(colon+1).trim());
+//				}
+//			}
+//			
+//			if(item instanceof GuideItem)
+//			{
+//				//reads image path
+//				while(true)
+//				{
+//					line = reader.readLine();
+//					if(line == null || line.equals(""))
+//						break;
+//					line = line.trim();
+//					int colon = line.indexOf(":");
+//					((GuideItem) item).setImagePath(line.substring(colon+1).trim());
+//				}
+//			}
+//			
+//			if(item instanceof HealingItem)
+//			{
+//				//reads strength
+//				while(true)
+//				{
+//					line = reader.readLine();
+//					if(line == null || line.equals(""))
+//						break;
+//					line = line.trim();
+//					int colon = line.indexOf(":");
+//					((HealingItem)item).setStrength(line.substring(colon+1).trim());
+//				}
+//			}
+//			
+//			if(item instanceof InventoryItem)
+//			{
+//				//reads capacity
+//				while(true)
+//				{
+//					line = reader.readLine();
+//					if(line == null || line.equals(""))
+//						break;
+//					line = line.trim();
+//					int colon = line.indexOf(":");
+//					((InventoryItem)item).setCapacity(line.substring(colon+1).trim());
+//				}
+//			}
+//			
+//			if(item instanceof KeyItem)
+//			{
+//				//reads associated monster
+//				while(true)
+//				{
+//					line = reader.readLine();
+//					if(line == null || line.equals(""))
+//						break;
+//					line = line.trim();
+//					int colon = line.indexOf(":");
+//					((KeyItem)item).setAssociatedMonster(line.substring(colon+1).trim());
+//				}
+//			}
+//			
+//			if(item instanceof TradableItem)
+//			{
+//				//reads NPC's id
+//				while(true)
+//				{
+//					line = reader.readLine();
+//					if(line == null || line.equals(""))
+//						break;
+//					line = line.trim();
+//					int colon = line.indexOf(":");
+//					((TradableItem)item).setNPC(line.substring(colon+1).trim());
+//				}
+//			}
+//			
+//			if(item instanceof WeaponItem)
+//			{
+//				//reads usage
+//				while(true)
+//				{
+//					line = reader.readLine();
+//					if(line == null || line.equals(""))
+//						break;
+//					line = line.trim();
+//					int colon = line.indexOf(":");
+//					((WeaponItem)item).setAttack(line.substring(colon+1).trim());
+//				}
+//			}
+//			
+//		}catch (IOException ex)
+//		{
+//			ex.getMessage();
+//		}catch (NumberFormatException ex)
+//		{
+//			ex.getMessage();
+//		}
+//		
+//		return (T) item;
+//	}
+	
 	//getters and setters
 	public String getItemID()
 	{
